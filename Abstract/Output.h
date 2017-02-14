4dfdf0a859ea907ffe18cdb03afca30d9d0615e1
Marcin Zyla <marcin.zyla0@gmail.com>
2017-02-14 02:34:22 +0100

    Fix abstract constructors
    Now abstracts can be derived from without modyfying .h files

--- Abstract/Output.h
+++ Abstract/Output.h
@@ -5,7 +5,7 @@ class AbstractOutput
 {
 public:
 	/* param like filename (in case of stdout may be additional message to print)*/
-	AbstractOutput(std::string param);
+	AbstractOutput(std::string param) {}; 
 	/* passes full string to output */
 	virtual void outString(std::string msg) = 0;
 };
