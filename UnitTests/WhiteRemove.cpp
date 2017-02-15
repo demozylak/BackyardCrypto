8091ac63cf9f7b9610789638942dcad29efffcaa
Marcin Zyla <marcin.zyla0@gmail.com>
2017-02-15 18:04:39 +0100

    Add WhiteRemove tests

---
+++ UnitTests/WhiteRemove.cpp
@@ -0,0 +1,43 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../Ciphers/WhiteRemove.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+
+namespace UnitTests
+{		
+	TEST_CLASS(WhiteRemoveTest)
+	{
+	public:
+		
+		TEST_METHOD(Constructor)
+		{
+			WhiteRemove *t = new WhiteRemove();
+		}
+
+		TEST_METHOD(GetName)
+		{
+			WhiteRemove *t = new WhiteRemove();
+			string ret = t->getName();
+
+
+			Assert::AreEqual((string)"WhiteRemove", ret, L"GetName() returns wrong name!");
+		}
+
+		TEST_METHOD(Encoder)
+		{
+			WhiteRemove *t = new WhiteRemove();
+			string ret = t->encrypt("Ala ma kota");
+
+			Assert::AreEqual((string)"Alamakota", ret);
+
+			ret = t->encrypt("Ala ma kota\na kot ma ale");
+
+			Assert::AreEqual((string)"Alamakotaakotmaale", ret);
+
+		}
+	};
+}
