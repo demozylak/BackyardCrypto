77b187a5b1cc279a09d8b265ea65485a0bedf508
Aerfio <mati6095@gmail.com>
2017-02-16 01:05:16 +0100

    now it works, i think

--- mathalphabet.cpp
+++ mathalphabet.cpp
@@ -1,5 +1,6 @@
-#include <assert.h>
+
 #include "MathAlphabet.h" //i made this file MANUALLY inside /Ciphers, why the hell does it appear to be in main folder?
+#include <assert.h>
 using namespace std;
 MathAlphabet::MathAlphabet()
 {
@@ -16,19 +17,16 @@ void MathAlphabet::setKey(Key * key)
 }
 
 
+
 string MathAlphabet::getName() {
 	return "MathAlphabet";
 }
 
-string MathAlphabet::decrypt(string message)
-{
-	return string();
-}
 
 string MathAlphabet::encrypt(string message)
 {
 	/*does NOT encrypt any sign unless it is: a b c d e f g h i j k l m o n r s t u v w x y z*/
-
+	/*idk whether i should use whiteremove here or use sth i did myself, will check out later*/
 
 	/*changes every letter from the message into another letter described in the encryption key
 	if the encryption key is eg alphabet but read from the end, then it looks like this:
@@ -36,23 +34,51 @@ string MathAlphabet::encrypt(string message)
 	z y x w v u t s r q p o n m l k j i h g f e d c b a <----- provided key, it'll surely be something entirely different
 	every "a" changes into "z", every "d" into "w" et cetera*/
 	string alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	string klucz = key->getPassword(); //as I understood, this password is the same as the key that will be needed
-	for (int i = 0; i < klucz.size(); i++) {
+	string klucz = key->getPassword();
+	assert(alfabet.length() == klucz.length());   //kinda obvious what it does; the problem is whether we should immidiately stop program (assert), or do something else - to be discussed
+	for (int i = 0; i < klucz.length(); i++) {
 		klucz.at(i) = toupper(klucz.at(i));				//casts klucz to uppercase
 	}
-	for (int i = 0; i < message.length; i++)
+	for (int i = 0; i < message.length(); i++)
 	{
 		message.at(i) = toupper(message.at(i));
 	}
 
-	assert(alfabet.length == klucz.length);   //kinda obvious what it does; the problem is whether we should immidiately stop program (assert), or do something else - to be discussed
-
+	string str;
+	string answer = "";
 	int temp;
-	for (int i = 0; i < message.length; i++)
+	for (int i = 0; i < message.length(); i++)
 	{
-		temp = klucz.find(message.at(i));
-		message.at(i) = klucz.at(temp);
+		temp = alfabet.find(message.at(i));
+		str = string(1, klucz.at(temp));
+		answer.append(str);
 	}
 	//need to make unit test here to check if this works
-	return message;
+	return answer;
+}
+
+string MathAlphabet::decrypt(string message)
+{
+	string klucz = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	string alfabet = key->getPassword();
+	assert(alfabet.length() == klucz.length());   //kinda obvious what it does; the problem is whether we should immidiately stop program (assert), or do something else - to be discussed
+	for (int i = 0; i < klucz.length(); i++) {
+		klucz.at(i) = toupper(klucz.at(i));				//casts klucz to uppercase
+	}
+	for (int i = 0; i < message.length(); i++)
+	{
+		message.at(i) = toupper(message.at(i));
+	}
+
+	string str;
+	string answer = "";
+	int temp;
+	for (int i = 0; i < message.length(); i++)
+	{
+		temp = alfabet.find(message.at(i));
+		str = string(1, klucz.at(temp));
+		answer.append(str);
+	}
+	//need to make unit test here to check if this works
+	return answer;
 }
