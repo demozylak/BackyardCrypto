2c2f8737142224d9ffb3d8752fc47054713bea6b
Marcin Zyla <marcin.zyla0@gmail.com>
2017-02-17 13:28:50 +0100

    Fix Iterator returning reference

--- Base/Iterator.h
+++ Base/Iterator.h
@@ -7,8 +7,8 @@ class that is returned from iterable and is used to access elements
 template <typename T> class Iterator
 {
 public:
-	Iterator()
-	T next(); // fetch the next element
+	Iterator();
+	T *next(); // fetch the next element
 	bool end(); // does the next element exist ?
 	
 
