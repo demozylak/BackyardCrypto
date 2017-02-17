d19676b50a515b531be014c99f6b8f01009fa1d4
Aerfio <mati6095@gmail.com>
2017-02-17 23:56:59 +0100

    added FileOutput.cpp and .h

---
+++ FileOutput.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Abstract\Output.h"
+#include <iostream>
+#include <fstream>
+class FileOutput :
+	public AbstractOutput
+{
+protected:
+	std::string param;
+	ofstream plik;
+public:
+	//param is path to file saved in .txt; don't append ".txt" to filename    
+	FileOutput(std::string param); 
+	~FileOutput();
+
+	void outString(std::string msg);
+};
+
