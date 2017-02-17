8bc89a4c47dd5a0489e29dc032d2539ab1b5799f
Marcin Zyla <marcin.zyla0@gmail.com>
2017-02-17 19:42:29 +0100

    Fix Iterators now pure virtual

--- Base/Iterator.h
+++ Base/Iterator.h
@@ -7,9 +7,9 @@ class that is returned from iterable and is used to access elements
 template <typename T> class Iterator
 {
 public:
-	Iterator();
-	T *next(); // fetch the next element
-	bool end(); // does the next element exist ?
+	//Iterator();
+	virtual T next() = 0; // fetch the next element
+	virtual bool isEnd() = 0; // does the next element exist ?
 	
 
 };
