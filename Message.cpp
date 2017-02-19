55208bebd424c1095753de6ae9ba5fa0956d3a57
rhmilenkowicz <rhmilenkowicz@gmail.com>
2017-02-19 21:34:35 +0100

    Add files via upload

---
+++ Message.cpp
@@ -0,0 +1,37 @@
+
+
+#include "Base\Message.h"
+
+
+using namespace std;
+
+
+Message::Message(AbstractInput *input){
+
+	msg = input->getString();
+
+}
+
+/* pass message to output */
+Message* Message::outMessage(AbstractOutput *output){
+
+	output->outString(msg);
+	return this;
+}
+
+
+
+
+/* visitor pattern */
+Message* Message::encrypt(AbstractCipher *cipher){
+
+	msg = cipher->encrypt(msg);
+	return this;
+}
+
+
+Message* Message::decrypt(AbstractCipher *cipher){
+
+	msg = cipher->encrypt(msg);
+	return this;
+}
