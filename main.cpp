a3ff0a84b8ed79e37cea6ead822db7a818467257
Marcin Zyla <marcin.zyla0@gmail.com>
2017-02-19 18:35:55 +0100

    Revert main to 'f362076'

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/* BackyardCrypto.exe (-e|-d) [messagefile] [keyfile] [-o file] 
+/* BackyardCrypto.exe (-e|-d) [messagefile] [keyfile] [-o file]
 
 eg
 BackyardCrypto.exe -e msg.txt -o kupa.txt			# asks for key (stdin), encrypt, out to kupa.txt
@@ -10,7 +10,7 @@ BackyardCrypto.exe -e msg.txt key.txt				# encrypt, stdout
 #include <string>
 
 #include "Abstract\Input.h"
-//#include "FileInput.h"
+#include "FileInput.h"
 #include "Base/KeyReader.h"
 #include "Base/Iterator.h"
 #include "Base/CipherLocator.h"
@@ -18,35 +18,49 @@ BackyardCrypto.exe -e msg.txt key.txt				# encrypt, stdout
 #include "Base/Message.h"
 #include "StdOutput.h"
 
-#include <string>
-#include <iostream>
+
 using namespace std;
 
 int main(int argc, char** argv)
 {
+	string message_filename = "msg.txt";
+	string key_filename = "key.txt";
+	string out_filename = "";
 
-	class StdIn:public AbstractInput{
-		public:
-		StdIn(string a):AbstractInput("") {};
-		string getString() {
-			return "abc\nklucz1\nbca\nklucz2";
-		}
-	};
-
-	AbstractInput *inq = new StdIn("");
+	//Validate args
+	//Check if params are correct(show help otherwise)
+	//set variables like  (empty string if no param given)
+	//string message_filename, key_filename, out_filename
 
 
-	KeyReader *r = new KeyReader(inq);
 
-	Iterator<Key*> *it = r->getIterator();
-	while (!it->isEnd()) 
+	//Interpret args and create KeyReader and Message
+	AbstractInput *msg_inp = new FileInput(message_filename);
+	AbstractInput *key_inp = new FileInput(key_filename);
+	AbstractOutput *output = new StdOutput("crypto msg");
+
+	Message *msg = new Message(msg_inp);
+	KeyReader *kr = new KeyReader(key_inp);
+
+
+
+
+	//get iterator from KeyReader, iterate over it, decipher message
+	Iterator<Key> *it = kr->getIterator();
+
+	CipherLocator *locator = new CipherLocator;
+
+	while (!it->end())
 	{
-		Key * a = it->next();
-		cout << a->getMethod() << " : " << a->getPassword() << endl;
+		Key *k = it->next(); // get next key
+		AbstractCipher *cipher = locator->getCipher(k); // get cipher
+
+		msg->encrypt(cipher); // visit the message
 	}
 
+	//out Message depending on args
+	msg->outMessage(output);
 
-	delete r;
 
 	return 0;
 }
